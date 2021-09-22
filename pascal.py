503d09b309d73ce9a705857165f5d37da57cb496
jr <jr@jirouette.com>
2021-09-23 00:43:20 +0200

    Moved voice mechanisms into voice module + Added un/ban commands

--- pascal.py
+++ pascal.py
@@ -2,74 +2,19 @@
 #coding: utf8
 
 import os
-import time
-import discord
 from discord.ext import commands
 from voice import Voice
-
-WEBHOOKS = dict()
-TEMP_BANLIST = dict()
 
 class Pascal(commands.Bot):
     async def on_ready(self):
         print('Now connected as {0.user.name}'.format(self))
         print('Ready. ')
 
-async def ban(ctx, author):
-    await ctx.send("<:ban:792391057706975272>")
-    TEMP_BANLIST[author.id] = time.time()
-
-async def is_in_voice_channel(ctx):
-    if not voice.client:
-        return True
-    if ctx.author.id in [member.id for member in voice.client.channel.members]:
-        return True
-    await ban(ctx, ctx.author)
-    return False
-
-async def is_coming_from_text_channel(ctx):
-    if ctx.channel.id == int(os.environ.get('TEXT_CHANNEL')):
-        return True
-    if not ctx.guild:
-        await ctx.send('🤫')
-    return False
-
-async def is_not_banned(ctx):
-    ban_duration = int(os.environ.get('TEMP_BAN_DURATION', 300))
-    print("banlist",TEMP_BANLIST)
-    if TEMP_BANLIST.get(ctx.author.id, 0) >= (time.time()-ban_duration):
-        await ctx.send("<:ban:792391057706975272>")
-        return False
-    banned_role = int(os.environ.get('BANNED_ROLE'))
-    if banned_role in [role.id for role in ctx.author.roles]:
-        await ctx.send("<:ban:792391057706975272>")
-        return False
-    return True
+    async def on_command_error(self, ctx, error):
+        pass
 
 if __name__ == '__main__':
     print("Starting Pascal...")
     bot = Pascal(command_prefix="!")
     voice = Voice(bot)
-
-    @bot.command()
-    @commands.check(is_coming_from_text_channel)
-    @commands.check(is_in_voice_channel)
-    @commands.check(is_not_banned)
-    async def music(ctx, *args):
-        text = " ".join(args)
-        print(text)
-        if (text.startswith("https://www.youtube.com/") or
-           text.startswith("https://youtube.com/") or
-           text.startswith("http://www.youtube.com/") or
-           text.startswith("http://youtube.com/") or
-           text.startswith("https://youtu.be/") or
-           text.startswith("http://youtu.be/")):
-            await voice.youtube(ctx, text)
-            return await ctx.send('🎵')
-        URL = voice.search(text)
-        if URL:
-            await voice.youtube(ctx, URL)
-            return await ctx.send('🎵 '+URL)
-        return await ctx.send('😔')
-    
     bot.run(os.environ.get('DISCORD_TOKEN'))
--- voice.py
+++ voice.py
@@ -3,8 +3,14 @@
 
 import discord
 import youtube_dl
+import time
+import typing
 import os
+from discord.ext import commands
 from googleapiclient.discovery import build
+
+TEMP_BANLIST = dict()
+ADMIN_ROLE = int(os.environ.get('ADMIN_ROLE'))
 
 youtube = build('youtube', 'v3', developerKey=os.environ.get('YOUTUBE_TOKEN'))
 # Suppress noise about console usage from errors
@@ -40,7 +46,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.url = data.get('url')
 
     @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
+    async def from_url(cls, url: str, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
 
@@ -52,19 +58,21 @@ class YTDLSource(discord.PCMVolumeTransformer):
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 class Voice(object):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.client = None
         if not discord.opus.is_loaded():
             discord.opus.load_opus("libopus.so")
+        bot.voice = self
+        init(bot)
     
-    def search(self, text):
+    def search(self, text: str) -> typing.Optional[str]:
         response = youtube.search().list(q=text, part='id,snippet', maxResults=1).execute()
         for result in response.get('items', []):
             return "https://www.youtube.com/watch?v=%s" % result['id']['videoId']
         return None
 
-    async def youtube(self, ctx, URL):
+    async def youtube(self, ctx: commands.Context, URL: str) -> None:
         if self.client is None or not self.client.is_connected():
             for channel in ctx.channel.guild.voice_channels:
                 if channel.id == int(os.environ.get('VOICE_CHANNEL')):
@@ -74,7 +82,77 @@ class Voice(object):
         await self.stop()
         self.client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
-    async def stop(self, *_, **__):
+    async def stop(self, *_, **__) -> None:
         if self.client is None or not self.client.is_connected():
             return
         self.client.stop()
+
+async def add_ban(ctx: commands.Context, author: discord.Member, duration = -1) -> None:
+    if duration < 0:
+        duration = int(os.environ.get('TEMP_BAN_DURATION', 300))
+    TEMP_BANLIST[author.id] = time.time()+duration
+
+async def is_in_voice_channel(ctx: commands.Context) -> bool:
+    if not ctx.bot.voice.client:
+        return True
+    if ctx.author.id in [member.id for member in ctx.bot.voice.client.channel.members]:
+        return True
+    await add_ban(ctx, ctx.author)
+    await ctx.send("<:ban:792391057706975272>")
+    return False
+
+async def is_coming_from_text_channel(ctx: commands.Context) -> bool:
+    if ctx.channel.id == int(os.environ.get('TEXT_CHANNEL')):
+        return True
+    if not ctx.guild:
+        await ctx.send('🤫')
+    return False
+
+async def is_not_banned(ctx: commands.Context) -> bool:
+    if TEMP_BANLIST.get(ctx.author.id, 0) >= time.time():
+        await ctx.send("<:ban:792391057706975272>")
+        return False
+    banned_role = int(os.environ.get('BANNED_ROLE'))
+    if banned_role in [role.id for role in ctx.author.roles]:
+        await ctx.send("<:ban:792391057706975272>")
+        return False
+    return True
+
+def init(bot: commands.Bot) -> None:
+    @bot.command()
+    @commands.check(is_coming_from_text_channel)
+    @commands.check(is_in_voice_channel)
+    @commands.check(is_not_banned)
+    async def music(ctx: commands.Context, *args) -> None:
+        text = " ".join(args)
+        print(text)
+        if (text.startswith("https://www.youtube.com/") or
+           text.startswith("https://youtube.com/") or
+           text.startswith("http://www.youtube.com/") or
+           text.startswith("http://youtube.com/") or
+           text.startswith("https://youtu.be/") or
+           text.startswith("http://youtu.be/")):
+            await ctx.bot.voice.youtube(ctx, text)
+            return await ctx.send('🎵')
+        URL = ctx.bot.voice.search(text)
+        if URL:
+            await ctx.bot.voice.youtube(ctx, URL)
+            return await ctx.send('🎵 '+URL)
+        await ctx.send('😔')
+
+    @bot.command()
+    @commands.check(is_coming_from_text_channel)
+    @commands.has_role(ADMIN_ROLE)
+    async def unban(ctx: commands.Context, member: typing.Optional[discord.Member]) -> None:
+        if not member:
+            TEMP_BANLIST.clear()
+        else:
+            del TEMP_BANLIST[member.id]
+        await ctx.send('👌')
+
+    @bot.command()
+    @commands.check(is_coming_from_text_channel)
+    @commands.has_role(ADMIN_ROLE)
+    async def ban(ctx: commands.Context, member: discord.Member, duration: typing.Optional[int] = -1) -> None:
+        await add_ban(ctx, member, duration*60)
+        await ctx.send('🔨')
