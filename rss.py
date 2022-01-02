7761ed3a3def24dac85777cfbf7ff82f1f76a0e2
jr <jr@jirouette.com>
2022-01-02 22:50:20 +0100

    Now using publishing date instead of GUID in RSS feed poster

--- rss.py
+++ rss.py
@@ -6,6 +6,7 @@ import asyncio
 import os
 import feedparser
 import redis
+from dateutil import parser
 from discord.ext import commands
 
 
@@ -13,19 +14,19 @@ class Rss(object):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.bot.loop.create_task(self.check_rss_and_send())
-    
+
     async def check_rss_and_send(self):
         channel = discord.Webhook.from_url(os.environ.get('RSS_WEBHOOK'), adapter=discord.RequestsWebhookAdapter())
         while True:
             news_feed = feedparser.parse(os.environ.get('RSS_URL'))
             r = redis.Redis(host='redis')
-            guid = r.get('LAST_ENTRY').decode('utf-8')
-            first_guid = None
+            last_entry_pub_date = parser.parse(r.get('LAST_ENTRY').decode('utf-8'))
+            pub_date = None
             for entry in news_feed.entries:
-                if first_guid is None:
-                    first_guid = entry.guid
-                if guid == entry.guid:
+                if pub_date is None:
+                    pub_date = entry.published
+                if last_entry_pub_date <= parser.parse(entry.published):
                     break
                 channel.send(entry.link, username=os.environ.get("RSS_USERNAME"), avatar_url=os.environ.get("RSS_AVATAR_URL"))
-            r.set('LAST_ENTRY', first_guid)
+            r.set('LAST_ENTRY', pub_date)
             await asyncio.sleep(60)
